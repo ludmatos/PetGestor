294366e4c62b875ad6df9b47515b1e06483a29a7
Ludmar <ludmatos@live.com>
2021-01-11 14:27:00 -0300

    price inserted on products table; utc=America/SaoPaulo

--- core/settings.py
+++ core/settings.py
@@ -119,7 +119,7 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'en-us'
 
 #TIME_ZONE = 'UTC'
-TIME_ZONE = 'America/Maceio'
+TIME_ZONE = 'America/SaoPaulo'
 
 USE_I18N = True
 
--- inventory/models.py
+++ inventory/models.py
@@ -4,9 +4,10 @@ class Stock(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30, unique=True)
     quantity = models.IntegerField(default=0)
-    category_tag = models.CharField(max_length=20, null=True)
-    NCM = models.IntegerField(null=False, default=0)
+    category_tag = models.CharField(max_length=20, blank=True, null=True)
+    NCM = models.CharField(max_length=8, default=00000000)
     bar_code = models.IntegerField(unique=True)
+    price = models.Decimal(decimal_places=2, max_digits=10000)
     is_deleted = models.BooleanField(default=False)
 
     def __str__(self):
